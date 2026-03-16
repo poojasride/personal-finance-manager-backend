459809a28b7db3fe07cc946e4862b1516a907acd
Pooja <poojasri.s@skematix.in>
2026-03-16 12:04:12 +0530

    updates

--- routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAIInsight } from "../controllers/aiController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/insight", protect, getAIInsight);
-
-export default router;
