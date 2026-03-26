66632c0f4f25be7d2dcbff7086bdc1e79964c735
Pooja <poojasri.s@skematix.in>
2026-03-26 15:50:18 +0530

    updates

--- routes/budgetRoutes.js
+++ routes/budgetRoutes.js
@@ -1,26 +1,22 @@
 import express from "express";
-
 import {
   createBudget,
   getBudgets,
+  getBudgetSummary,
   getBudgetById,
   updateBudget,
   deleteBudget,
-  getBudgetSummary
 } from "../controllers/budgetController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createBudget);
-
-router.get("/", getBudgets);
-
-router.get("/summary", getBudgetSummary);
-
-router.get("/:id", getBudgetById);
-
-router.put("/:id", updateBudget);
-
-router.delete("/:id", deleteBudget);
+router.post("/", protect, createBudget);
+router.get("/", protect, getBudgets);
+router.get("/summary", protect, getBudgetSummary);
+router.get("/:id", protect, getBudgetById);
+router.put("/:id", protect, updateBudget);
+router.delete("/:id", protect, deleteBudget);
 
 export default router;
